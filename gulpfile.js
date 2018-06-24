5081bf1254579658c003713cd3da26b08e3b4f6c
knazark <knazark@tenantcloud.com>
2018-06-24 21:29:06 +0300

    fix: postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+// PostCss
+var postcss = require('gulp-postcss');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -16,12 +19,13 @@ gulp.task('serve', ['sass'], function() {
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
 	return gulp.src("app/scss/*.scss")
-		.pipe(sass())
+		.pipe(postcss().pipe(sass().on('error', sass.logError)))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'safari >= 7.1', 'ie >= 10', 'opera >= 20', 'ios 8', 'android 4.4.4'],
+			cascade: false
+		}))
 		.pipe(gulp.dest("app/css"))
 		.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
-
-// PostCss
-var postcss = require('gulp-postcss');
